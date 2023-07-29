bdac97f2f6e7e5fe50e866dafe236cf9fbc27541
wachirarobbinson <ngirigacharobbinson@gmail.com>
2023-07-29 10:46:54 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+const textbox = document.getElementById("create-task-form");
+textbox.addEventListener("submit", (e) => {
+	e.preventDefault();
+	buildToDo(e.target.new_task_description.value);
+});
+
+function buildToDo(e_target_new_task_description_value) {
+	const list = document.createElement("li");
+	const button = document.createElement("button");
+
+	list.textContent = `${e_target_new_task_description_value}  `;
+
+	button.textContent = "delete";
+
+	const unordered_list = document.getElementById("tasks");
+
+	unordered_list.appendChild(list);
+	list.appendChild(button);
+
+	button.addEventListener("click", deleteToDoItem);
+
+	function deleteToDoItem(list) {
+		return list.target.parentNode.remove();
+	}
+}
+
 });
